43e3b6891352efc2f658ee08e14aed1a51db02ac
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-06-20 18:36:41 +0100

    Added: appbar search icon, ScrollView, & ListView

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,44 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> words = [
+    'abc',
+    'abc',
+    'abc',
+    'abc',
+    'abc',
+    'abc',
+    'abc',
+    'abc',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('SW Dictionary'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: null,
+            tooltip: 'Search',
+          )
+        ],
       ),
-      body: Center(
+      body: Scrollbar(
+        child: ListView.builder(
+          itemBuilder: (ctx, index) => ListTile(
+            title: Text(words[index]),
+          ),
+        ),
       ),
     );
   }
